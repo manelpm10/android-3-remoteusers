6872e1745cacf432329ace492f88629694210f4f
Manel <yo@manelperez.com>
2018-06-06 10:24:15 +0200

    Optimized imports

--- app/src/main/java/es/pue/android/remoteusers/UsersActivity.java
+++ app/src/main/java/es/pue/android/remoteusers/UsersActivity.java
@@ -1,37 +1,22 @@
 package es.pue.android.remoteusers;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
 import es.pue.android.remoteusers.model.User;
 
